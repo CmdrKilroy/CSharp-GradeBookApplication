a35b5aa2f462cb4ba9fdd8b63d2b34448a6594f6
CmdrKilroy <61610254+CmdrKilroy@users.noreply.github.com>
2020-02-29 21:55:00 -0700

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,54 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var cutoff = (int)Math.Truncate(Students.Count * 0.2);
+
+            var averageGrades =
+                (from student in Students
+                orderby student.AverageGrade descending
+                select student.AverageGrade).ToList();
+
+            var classPosition = averageGrades.IndexOf(averageGrade);
+            
+            if (classPosition < cutoff)
+            {
+                return 'A';
+            }
+            else
+            {
+                classPosition -= cutoff;
+                if (classPosition < cutoff)
+                {
+                    return 'B';
+                }
+                else
+                {
+                    classPosition -= cutoff;
+                    if (classPosition < cutoff)
+                    {
+                        return 'C';
+                    }
+                    else
+                    {
+                        classPosition -= cutoff;
+                        if (classPosition < cutoff)
+                        {
+                            return 'D';
+                        }
+                    }
+                }
+            }
+
+            return 'F';
         }
     }
 }
